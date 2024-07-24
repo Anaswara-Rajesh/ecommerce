4e6e79c68c34fe48a515677b175897fd3f880789
anaswara rajesh <anaswara.rajesh@lanwaresolutions.com>
2024-07-24 17:17:40 +0530

    set up project and authentication

---
+++ ecommerce-backend/controllers/authController.js
@@ -0,0 +1,61 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+exports.registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    const token = generateToken(user);
+
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password, "mmmm");
+  try {
+    const user = await User.findOne({ email });
+    console.log(user, "nnnnnnnnnnnnnnnnnnnnnn");
+    if (user) {
+      const token = generateToken(user);
+      console.log(token,"token");
+      console.log(token, "token>>>>>>>>>>>.....");
+      res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        token,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
---
+++ ecommerce-backend/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { protect } = require ("../middlewares/authMiddleware");
+const { registerUser, loginUser } = require("../controllers/authController");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+router.post("/login", protect, loginUser);
+
+module.exports = router;
